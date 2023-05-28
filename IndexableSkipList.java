7a8772ea5a376fdc2e1748d24fb2f2e731eab922
Gilad Gochman <ggochman@gmail.com>
2023-05-28 20:27:10 +0300

    2.2.1 Implementation of Abstract Functions
    added find and generateHeight functions.
    added credits

--- IndexableSkipList.java
+++ IndexableSkipList.java
@@ -7,12 +7,26 @@ public class IndexableSkipList extends AbstractSkipList {
 
     @Override
     public Node find(int val) {
-        throw new UnsupportedOperationException("Replace this by your implementation");
+    	int height = head.height();
+        Node current = head;
+        for (int level = height; level >= 0 ; level--) {
+            while (current.getNext(level).key() < val) {
+            	//no need to check if has next because val cannot be larger than max_value and if it equals than current becomes the tail.
+                current = current.getNext(level);
+            }
+        }
+        return current;
+    	
     }
 
     @Override
     public int generateHeight() {
-        throw new UnsupportedOperationException("Replace this by your implementation");
+    	int height=1;
+    	while (Math.random() > probability) {
+    		//for example for probability 0.1: if the random number is bigger than 0.1 the height will increase, therefore there is a probability of 0.1 to end the process.
+    		height++;
+    	}
+    	return height;
     }
 
     public int rank(int val) {
