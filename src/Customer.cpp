04004078c7bcd7966e2d1960a7a81b7a870e974c
ClilArgas <clilargas23@gmail.com>
2024-01-17 22:15:32 +0200

    finished Customer class

--- src/Customer.cpp
+++ src/Customer.cpp
@@ -6,7 +6,9 @@ using std::string;
 using std::vector;
 
 
-Customer::Customer(int id, const string &name, int locationDistance, int maxOrders) : id(id), name(name), locationDistance(locationDistance), maxOrders(maxOrders){}
+Customer::Customer(int id, const string &name, int locationDistance, int maxOrders) : id(id), name(name), locationDistance(locationDistance), maxOrders(maxOrders){
+    ordersId=vector<int>();
+}
 
 const string &Customer::getName() const {
     return name;
@@ -34,4 +36,24 @@ bool Customer:: canMakeOrder() const{
 
 const vector<int> &Customer::getOrders() const {
      return ordersId;
+}
+
+int Customer::addOrder(int orderId){
+    if(canMakeOrder){
+        ordersId.push_back(orderId);
+        return orderId;
+    }
+    return -1;
+}
+
+SoldierCustomer :: SoldierCustomer(int id, string name, int locationDistance, int maxOrders) : Costumer(id, name, locationDistance, maxOrders){};
+
+SoldierCustomer *SoldierCustomer::clone(){
+   return new SoldierCustomer(*this)
+}
+
+CivilianCustomer :: CivilianCustomer(int id, string name, int locationDistance, int maxOrders) : Costumer(id, name, locationDistance, maxOrders){};
+
+CivilianCustomer *CivilianCustomer::clone(){
+   return new CivilianCustomer(*this)
 }
