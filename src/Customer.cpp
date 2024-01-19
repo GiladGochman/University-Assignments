1b262e5b753396372c8c5cd948ec878dfc69de05
ClilArgas <clilargas23@gmail.com>
2024-01-19 09:45:01 +0200

    finished customer

--- src/Customer.cpp
+++ src/Customer.cpp
@@ -6,7 +6,7 @@ using std::string;
 using std::vector;
 
 
-Customer::Customer(int id, const string &name, int locationDistance, int maxOrders) : id(id), name(name), locationDistance(locationDistance), maxOrders(maxOrders), {
+Customer::Customer(int id, const string &name, int locationDistance, int maxOrders) : id(id), name(name), locationDistance(locationDistance), maxOrders(maxOrders) {
     ordersId=vector<int>();
 }
 
@@ -31,29 +31,28 @@ int Customer:: getNumOrders() const{
 }
 
 bool Customer:: canMakeOrder() const{
-  return getNumOrders()<=maxOrders;
+  return getNumOrders() < maxOrders;
 }
-
-vector<int> &Customer::getOrders() const {
+const vector<int> &Customer::getOrdersId() const {
      return ordersId;
 }
 
 int Customer::addOrder(int orderId){
-    if(canMakeOrder){
+    if(canMakeOrder()){
         ordersId.push_back(orderId);
         return orderId;
     }
     return -1;
 }
 
-SoldierCustomer :: SoldierCustomer(int id, string name, int locationDistance, int maxOrders) : Costumer(id, name, locationDistance, maxOrders){};
+SoldierCustomer :: SoldierCustomer(int id, string name, int locationDistance, int maxOrders): Customer(id, name, locationDistance, maxOrders) {}
 
 SoldierCustomer *SoldierCustomer::clone() const{
-   return new SoldierCustomer(*this)
+   return new SoldierCustomer(*this);
 }
 
-CivilianCustomer :: CivilianCustomer(int id, string name, int locationDistance, int maxOrders) : Costumer(id, name, locationDistance, maxOrders){};
+CivilianCustomer :: CivilianCustomer(int id, string name, int locationDistance, int maxOrders): Customer(id, name ,locationDistance, maxOrders) {}
 
 CivilianCustomer *CivilianCustomer::clone() const{
-   return new CivilianCustomer(*this)
+   return new CivilianCustomer(*this);
 }
