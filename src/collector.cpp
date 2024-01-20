4c9a2772ff1ab6380e245838f56adeb7b0271d3a
ClilArgas <clilargas23@gmail.com>
2024-01-20 11:02:31 +0200

    started working on collector class

---
+++ src/collector.cpp
@@ -0,0 +1,52 @@
+#include <vector>
+#include <string>
+#include "../include/Volunteer.h"
+#include "../include/Order.h"
+
+CollectorVolunteer::CollectorVolunteer(int id, string name, int coolDown) : Volunteer(id,name), coolDown(coolDown), timeLeft(0) {}
+
+CollectorVolunteer *CollectorVolunteer::clone() const{
+    return new CollectorVolunteer(*this);
+}
+
+int CollectorVolunteer::getCoolDown() const{
+    return coolDown;
+}
+
+int CollectorVolunteer::getTimeLeft() const{
+    return timeLeft;
+}
+
+bool CollectorVolunteer::decreaseCoolDown() {
+    if(timeLeft>0){
+        timeLeft = timeLeft - 1;
+        return true;
+    }
+    return false;
+
+}
+
+bool CollectorVolunteer::canTakeOrder(const Order &order) const{
+    if(isBusy()) return false;
+    return true;
+
+}
+
+void CollectorVolunteer:: acceptOrder(const Order &order){
+    timeLeft=coolDown;
+    activeOrderId=order.getId(); 
+}
+
+void CollectorVolunteer:: step(){
+    if(isBusy()){
+        decreaseCoolDown();
+        if(getTimeLeft()==0){
+            completedOrderId=activeOrderId;
+            activeOrderId=NO_ORDER;
+        }
+    }
+    else{
+        acceptOrder()
+    }
+}
+
