f66b33cfa6cebb487bc8fa62d3cf7fb79d640903
clila <clilargas23@gmail.com>
2024-02-21 10:46:28 +0200

    fixed removed all cards from table

--- src/main/java/bguspl/set/ex/Dealer.java
+++ src/main/java/bguspl/set/ex/Dealer.java
@@ -281,7 +281,11 @@ public class Dealer implements Runnable {
       //TODO syncronize tokensCounter
       player.tokensCounter = 0;
     }
+    players[playerWhoClaimedSet].foundSet=false;
+    players[playerWhoClaimedSet].getPlayerThread().interrupt();
+    playerWhoClaimedSet=-1;
     if(!shouldFinish())  placeCardsOnTable();
+    
    
     table.lock.writeLock().unlock();
   }
